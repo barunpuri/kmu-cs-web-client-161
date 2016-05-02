f0bf26ef9105ae160853755fee8267f07c4eb563
barunpuri <wjdrms1388@gmail.com>
2016-05-02 19:14:04 +0900

    index add

---
+++ login.js
@@ -0,0 +1,13 @@
+
+function login(id, pw){
+    //alert(id + " " + pw);
+    //alert(typeof(id) + " " + typeof(pw))
+    if( ( id == "barunpuri" )&&( pw == "20153235" ) ){
+        //alert("shit");
+        window.location.href="pages/gamelist.html";
+    }
+    else
+        alert("wrong user data");
+            
+}
+
